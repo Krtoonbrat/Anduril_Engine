2f2b0d78e5c8c16210486a6f74985e4e2774a6bc
Kevin Hughes <kjhughes4@wisc.edu>
2024-04-17 22:15:19 -0500

    Larger evasion capture bonus

--- MovePicker.cpp
+++ MovePicker.cpp
@@ -121,7 +121,7 @@ void MovePicker::score() {
         else { // evasions
             if (board.is_capture_move(m)) {
                 // the condition is for enpassant, as there would not be a piece on the destination
-                m.score = libchess::Position::pieceValuesMG[(board.piece_type_on(m.to_square()) ? board.piece_type_on(m.to_square())->value() : 0)] + (100000); // we add a large number to make sure captures are always searched first
+                m.score = libchess::Position::pieceValuesMG[(board.piece_type_on(m.to_square()) ? board.piece_type_on(m.to_square())->value() : 0)] + (500000); // we add a large number to make sure captures are always searched first
             }
             else {
                 m.score = moveHistory->at(board.side_to_move()).at(m.from_square()).at(m.to_square())
